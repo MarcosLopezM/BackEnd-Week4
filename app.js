dda5846d0e211d5067916d1040005067904ec0b4
MarcosLopez <marcoslm@ciencias.unam.mx>
2022-04-26 09:30:42 -0500

    Requirement 1. Step 3: Understanding the code

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const explorers = JSON.parse(rawdata);
 
 // Part 2: Get the quantity of explorers names in node
 const explorersInNode = explorers.filter((explorer) => explorer.mission == "node");
-//console.log(explorersInNode.length)
+// console.log(explorersInNode.length)
 
 // Part4: Get the explorer's usernames in Node
 const explorersInNodeToGetUsernames = explorers.filter((explorer) => explorer.mission == "node");
@@ -15,7 +15,7 @@ const usernamesInNode = explorersInNodeToGetUsernames.map((explorer) => explorer
 
 // DEAD CODE: Part 5,6,7, please remove this and go to Part 8!
 
-// Part 5: Get a new list of explorers in node, if the score numbers is divisible by 3, I need a new propery called trick, and the value assigned is FIZZ, if not the value should be the score itself.
+// Part 5: Get a new list of explorers in node, if the score numbers is divisible by 3, I need a new property called trick, and the value assigned is FIZZ, if not the value should be the score itself.
 // Score: 3, Trick: FIZZ.
 // Score: 4, Trick: 4.
 // Score: 5, Trick: 5.
