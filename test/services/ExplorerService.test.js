b66009fc085af4ddb3e1119e8c3cc32bea9a53e1
MarcosLopez <marcoslm@ciencias.unam.mx>
2022-04-26 17:11:33 -0500

    Step 7: Installing linter and fixing code

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -1,11 +1,11 @@
-const Reader = require('./../../lib/utils/Reader');
-const ExplorerService = require('./../../lib/services/ExplorerService');
+const Reader = require("./../../lib/utils/Reader");
+const ExplorerService = require("./../../lib/services/ExplorerService");
 
 describe("Test para ExplorerService", () => {
     test("1) Amount of explorer in certain mission", () => {
-        const explorers = Reader.readJsonFile('explorers.json');
-        const explorerInNode = ExplorerService.getAmountOfExplorersByMission(explorers, 'node');
+        const explorers = Reader.readJsonFile("explorers.json");
+        const explorerInNode = ExplorerService.getAmountOfExplorersByMission(explorers, "node");
 
-        expect(explorerInNode).toBe(10)
+        expect(explorerInNode).toBe(10);
     });
-})
+});
--- test/services/FizzBuzzService.test.js
+++ test/services/FizzBuzzService.test.js
@@ -1,10 +1,10 @@
-const FizzBuzzService = require('./../../lib/services/FizzBuzzService'); 
+const FizzBuzzService = require("./../../lib/services/FizzBuzzService"); 
 
 describe("Test for FizzBuzzService", () => {
     test("1) FizzBuzz dilema", () => {
         const explorer = {"score": 5};
         const result = FizzBuzzService.applyValidationInExplorer(explorer);
 
-        expect(result.trick).toMatch(/BUZZ/)
+        expect(result.trick).toMatch(/BUZZ/);
     });
-})
+});
