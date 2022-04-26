5541202c88f8276f5848ee19b2ce9ab7ee561906
MarcosLopez <marcoslm@ciencias.unam.mx>
2022-04-26 11:05:49 -0500

    Step 6: Refactoring FizzBuzzService and adding corresponding unit test

---
+++ test/services/FizzBuzzService.test.js
@@ -0,0 +1,10 @@
+const FizzBuzzService = require('./../../lib/services/FizzBuzzService'); 
+
+describe("Test for FizzBuzzService", () => {
+    test("1) FizzBuzz dilema", () => {
+        const explorer = {"score": 5};
+        const result = FizzBuzzService.applyValidationInExplorer(explorer);
+
+        expect(result.trick).toMatch(/BUZZ/)
+    });
+})
