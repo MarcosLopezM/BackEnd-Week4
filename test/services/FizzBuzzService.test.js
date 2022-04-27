557c7915b33b8831191e4c15af85a107ae7a178c
MarcosLopez <marcoslm@ciencias.unam.mx>
2022-04-27 17:06:31 -0500

    Project 3. Step 1: Creating applyValidationInNumber method and its corresponding unit test

--- test/services/FizzBuzzService.test.js
+++ test/services/FizzBuzzService.test.js
@@ -1,10 +1,23 @@
 const FizzBuzzService = require("./../../lib/services/FizzBuzzService"); 
 
 describe("Test for FizzBuzzService", () => {
-    test("1) FizzBuzz dilema", () => {
+    test("1) FizzBuzz dilemma", () => {
         const explorer = {"score": 5};
         const result = FizzBuzzService.applyValidationInExplorer(explorer);
 
         expect(result.trick).toMatch(/BUZZ/);
+    });
+
+    test("2) FizzBuzz dilemma for any number", () => {
+        const number1 = 15;
+        const result1 = FizzBuzzService.applyValidationInNumber(number1);
+        const number2 = 3;
+        const result2 = FizzBuzzService.applyValidationInNumber(number2);
+        const number3 = 5;
+        const result3 = FizzBuzzService.applyValidationInNumber(number3);
+
+        expect(result1).toMatch(/FIZZBUZZ/);
+        expect(result2).toMatch(/FIZZ/);
+        expect(result3).toMatch(/BUZZ/);
     });
 });
